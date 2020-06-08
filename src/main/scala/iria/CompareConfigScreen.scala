d6595d540b0fe51d6f1ebcb6634a77939fad4b68
Oleg Lukin <oleg.e.lukin@hotmail.com>
2020-06-08 16:36:32 +0300

    compare config screen enchancement add Browse buttons

--- src/main/scala/iria/CompareConfigScreen.scala
+++ src/main/scala/iria/CompareConfigScreen.scala
@@ -2,108 +2,141 @@ package iria
 
 import javafx.event.ActionEvent
 import iria.model.CompareConfig
-import scalafx.stage.Stage
+import scalafx.stage.{Stage, DirectoryChooser}
 import scalafx.scene.Scene
 import scalafx.scene.paint.Color
 import scalafx.scene.layout.GridPane
 import scalafx.scene.control.{Label, TextField, Button}
-import scalafx.stage.DirectoryChooser
+
 
 class CompareConfigScreen (val diffScreen: DiffScreen) {
 
-  def getPath(dc: DirectoryChooser, st: Stage, default: String): String = dc.showDialog(st) match {
-    case null => default
-    case v => v.getAbsolutePath
+  val stage: Stage = new Stage {
+    title = "Iria - Directory Compare"
+    maxWidth = 800
+    maxHeight = 360
+    minWidth = 550
+    minHeight = 230
   }
 
-  def left = CompareConfig.left.getOrElse("")
-  def right = CompareConfig.right.getOrElse("")
+  val errormsg = "Provide directory path"
+  val errorStyle = "-fx-text-inner-color: red;"
+  val normalStyle = "-fx-text-inner-color: black;"
 
-  val dirChoser = new DirectoryChooser { title = "Select folder to compare" }
+  val dirChoser = new DirectoryChooser { title = "Select directory to compare" }
 
-  val leftFolderLabel = new Label("Left")
-  val leftFolderText = new TextField {
-    text = left
-    onMouseClicked = (e: javafx.scene.input.MouseEvent) => {
-      CompareConfig.left = Some(getPath(dirChoser, stage, left))
-      text = left
-    }
-  }
-  val leftFolderMessage = new Label("Select left folder") {
-    id = "leftFolderMessage"
-    visible = false
-  }
+  val leftDirText = createDirText(errorStyle, normalStyle)
+  val rightDirText = createDirText(errorStyle, normalStyle)
+  val leftBrowseButton = createBrowseButton(leftDirText, normalStyle, dirChoser, left, stage)
+  val rightBrowseButton = createBrowseButton(rightDirText, normalStyle, dirChoser, right, stage)
 
-  val rightFolderLabel = new Label("Right")
-  val rightFolderText = new TextField {
-    text = right
-    onMouseClicked = (e: javafx.scene.input.MouseEvent) => {
-      CompareConfig.right = Some(getPath(dirChoser, stage, right))
-      text = right
-    }
-  }
-  val rightFolderMessage = new Label("Select right folder") {
-    id = "rightFolderMessage"
-    visible = false
-  }
 
   val compareButton = new Button("Compare") {
     onAction = (event: ActionEvent) => {
-      leftFolderText.getText match {
-        case "" => {
+      leftDirText.getText match {
+        case x if isInvalidPath(x) => {
           CompareConfig.left = None
-          leftFolderMessage.setVisible(true)
+          leftDirText.setStyle(errorStyle)
+          leftDirText.text = errormsg
         }
         case v => {
           CompareConfig.left = Some(v)
-          leftFolderMessage.setVisible(false)
         }
       }
 
-      rightFolderText.getText match {
-        case "" => {
+      rightDirText.getText match {
+        case x if isInvalidPath(x) => {
           CompareConfig.right = None
-          rightFolderMessage.setVisible(true)
+          rightDirText.setStyle(errorStyle)
+          rightDirText.text = errormsg
         }
         case v => {
           CompareConfig.right = Some(v)
-          rightFolderMessage.setVisible(false)
         }
       }
 
-      if (left != "" && right != "") {
+      if (!isInvalidPath(left) && !isInvalidPath(right)) {
         stage.hide
         diffScreen.displayElements
         diffScreen.stage.showAndWait
       }
     }
   }
+
   
-  val stage: Stage = new Stage {
-    title = "Iria - Folder Compare"
+  stage.scene = new Scene {
+    stylesheets.add("compareconfig.css")
+    fill = Color.rgb(38, 38, 38)
+    content = new GridPane {
+      id = "compareConfigGridPane"
 
-    maxWidth = 800
-    maxHeight = 360
-    minWidth = 550
-    minHeight = 230
-
-    scene = new Scene {
-      stylesheets.add("compareconfig.css")
-      fill = Color.rgb(38, 38, 38)
-      content = new GridPane {
-        id = "compareConfigGridPane"
-
-        add(new Label("Select folders to compare"), 1, 0)
-        add(leftFolderLabel, 0, 1)
-        add(leftFolderText, 1, 1)
-        add(leftFolderMessage, 2, 1)
-        add(rightFolderLabel, 0, 2)
-        add(rightFolderText, 1, 2)
-        add(rightFolderMessage, 2, 2)
-        add(compareButton, 1, 3)
-      }
+      add(new Label("Select directories to compare"), 1, 0)
+      add(new Label("Left"), 0, 1)
+      add(leftDirText, 1, 1)
+      add(leftBrowseButton, 2, 1)
+      add(new Label("Right"), 0, 2)
+      add(rightDirText, 1, 2)
+      add(rightBrowseButton, 2, 2)
+      add(compareButton, 1, 3)
     }
   }
 
   stage.show
+
+
+  def left: String = CompareConfig.left.getOrElse("")
+  def right: String = CompareConfig.right.getOrElse("")
+
+  /**
+    * Creates text field with a change listener (valid, error)
+    * @param errorStyle style for error message
+    * @param normalStyle normal (valid) text style
+    * @return new TextField node
+    */
+  def createDirText(errorStyle: String, normalStyle: String): TextField = {
+    val field = new TextField
+    
+    field.textProperty.addListener((observable, oldValue, newValue) => {
+      newValue match {
+        case x if isInvalidPath(x.toString) => field.setStyle(errorStyle)
+        case _ => field.setStyle(normalStyle)
+      }
+    })
+
+    field
+  }
+
+
+  def isInvalidPath(path: String): Boolean = Seq("", errormsg).contains(path) // TODO Consider improving path validation
+
+
+  def getPath(dc: DirectoryChooser, st: Stage, default: String): String = dc.showDialog(st) match {
+    case null => default
+    case v => v.getAbsolutePath
+  }
+
+
+  /**
+    * Creates bew Buttom adding directory choser and corresponding text field behaviour
+    * @param textField
+    * @param textStyle
+    * @param dirChoser
+    * @param defaultPath
+    * @param stage
+    * @return bew Buttom node
+    */
+  def createBrowseButton(
+    textField: TextField,
+    textStyle: String,
+    dirChoser: DirectoryChooser,
+    defaultPath: String,
+    stage: Stage
+    ): Button =
+    new Button("Browse") {
+      onAction = (event: ActionEvent) => {
+        CompareConfig.left = Some(getPath(dirChoser, stage, defaultPath))
+        textField.text = left
+        textField.setStyle(textStyle)
+      }
+    }
 }
